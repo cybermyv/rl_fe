db9a80f68b0053d593a39c1828b16bc9a90c8ea8
cybermyv <yuriymironov77@gmail.com>
2021-01-11 22:59:21 +0400

    [+] какркас приложения

---
+++ src/app/modules/main/main.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MainRoutingModule } from './main-routing.module';
+import { MainPageComponent } from './components/main-page/main-page.component';
+
+@NgModule({
+  declarations: [
+    MainPageComponent,
+  ],
+  imports: [
+    CommonModule,
+    MainRoutingModule,    
+  ]
+})
+export class MainModule { }
